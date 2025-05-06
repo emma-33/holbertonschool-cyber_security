6063b5f963effc4965feb79f39c670752d57bb79
Emma Lateyron <emma.lateyron@orange.fr>
2025-05-06 21:41:42 +0200

    1-xor_decoder.sh

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -5,11 +5,10 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-encoded_mess="$1"
 
-no_xor_encoded_mess=$(echo "$encoded_mess" | sed 's/{xor}//')
+encoded_mess=$(echo "$1" | sed 's/{xor}//')
 
-Base64_mess=$(echo "$no_xor_encoded_mess" | base64 -d | tr -d '\0')
+Base64_mess=$(echo "$encoded_mess" | base64 -d | tr -d '\0')
 
 key=95
 
@@ -17,8 +16,8 @@ decoded_message=""
 for (( i=0; i<${#Base64_mess}; i++ )); do
     char="${Base64_mess:$i:1}" 
     ascii_value=$(printf "%d" "'$char")
-    xor_char=$(($ascii_value ^ $key))
-    decoded_mess+="$(printf "\\x$(printf '%x' $xor_char)")"
+    xor_char=$((ascii_value ^ key))
+    decoded_mess+="$(printf "$(printf '\\x%x' $xor_char)")"
 done
 
-echo "$decoded_mess"
+echo -e "$decoded_mess"

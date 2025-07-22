d00bf5f06891ce2e689a6bbf06c7a669c9b4fd8d
Emma Lateyron <emma.lateyron@orange.fr>
2025-07-23 00:00:42 +0200

    [FIX] read_write_heap.py // pep8

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -13,8 +13,10 @@ replace_string: The string to replace the search_string with
 """
 import sys
 
+
 def find_and_replace_string(pid, search_string, replace_string):
-    """Finds a string in the heap memory of a process and replaces it with another string."""
+    """Finds a string in the heap memory of a process and replaces
+    it with another string."""
 
     try:
         with open(f"/proc/{pid}/maps", "r") as maps_file:
@@ -28,10 +30,11 @@ def find_and_replace_string(pid, search_string, replace_string):
 
         with open(f"/proc/{pid}/mem", "r+b") as mem_file:
             mem_file.seek(int(heap_address.split('-')[0], 16))
-            data = mem_file.read(1024) 
-            
+            data = mem_file.read(1024)
+
             if search_string.encode() in data:
-                new_data = data.replace(search_string.encode(), replace_string.encode())
+                new_data = data.replace(search_string.encode(),
+                                        replace_string.encode())
                 mem_file.seek(int(heap_address.split('-')[0], 16))
                 mem_file.write(new_data)
             else:
@@ -39,7 +42,7 @@ def find_and_replace_string(pid, search_string, replace_string):
     except FileNotFoundError:
         print(f"Process with PID {pid} does not exist.")
     except PermissionError:
-        print(f"Permission denied to access process {pid}. You may need to run as root.")
+        print(f"Permission denied to access process {pid}. Run as root.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -49,7 +52,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: read_write_heap.py pid search_string replace_string")
         sys.exit(1)
-    
+
     pid = sys.argv[1]
     search_string = sys.argv[2]
     replace_string = sys.argv[3]

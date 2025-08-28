959fea670255cd2ade58d9bd5d45f949c0c84203
Emma Lateyron <emma.lateyron@orange.fr>
2025-08-28 16:57:03 +0200

    [FIX] 4-write.rb // put statement

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -9,5 +9,5 @@ def merge_json_files(file1_path, file2_path)
 
     File.write(file2_path, JSON.pretty_generate(merged_data))
 
-    puts "Merged data written into #{file2_path}"
+    puts "Merged JSON written to #{file2_path}"
 end

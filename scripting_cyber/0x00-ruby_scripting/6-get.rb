10528f089f3e8be88dacff9df6a19b65d64a3d67
Emma Lateyron <emma.lateyron@orange.fr>
2025-08-28 15:06:18 +0200

    [FIX] 6-get.rb // add puts

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby
+require 'json'
 require 'net/http'
 require 'uri'
 
 def get_request(url)
     uri = URI(url)
     response = Net::HTTP.get_response(uri)
-    puts response.body if response.is_a?(Net::HTTPSuccess)
+
+    response_body = JSON.parse(response.body)
+
+    puts "Response status: #{response.code} #{response.message}"
+    puts "Response body:"
+    puts JSON.pretty_generate(response_body) if response.is_a?(Net::HTTPSuccess)
 end

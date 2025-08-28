e61418f4a02483b5cb4f1a635998e86f58683307
Emma Lateyron <emma.lateyron@orange.fr>
2025-08-28 15:16:18 +0200

    [FIX] 8-post.rb // add puts

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,9 +1,16 @@
 #!/usr/bin/env ruby
+
+require 'json'
 require 'net/http'
 require 'uri'
 
 def post_request(url, body_params)
     uri = URI(url)
     response = Net::HTTP.post_form(uri, body_params)
-    puts response.body if response.is_a?(Net::HTTPSuccess)
+
+    response_body = JSON.parse(response.body)
+
+    puts "Response status: #{response.code} #{response.message}"
+    puts "Response body:"
+    puts JSON.pretty_generate(response_body) if response.is_a?(Net::HTTPSuccess)
 end

